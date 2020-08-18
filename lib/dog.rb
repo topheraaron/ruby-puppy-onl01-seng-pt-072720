f4a8beeab5525f0faf1fe27ed3a4a630a4accde6
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-18 04:18:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def Dog.print_all
+    puts []
+  end
+end
